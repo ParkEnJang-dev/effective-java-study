2300eb7d78704af82d16d03af3392e288885871f
min-gui <msj349911@gmail.com>
2024-04-24 00:30:25 +0900

    item2 builder 패턴 써라

---
+++ src/chapter2/item2/min/NutritionFacts.java
@@ -0,0 +1,43 @@
+package chapter2.item2.min;
+
+public class NutritionFacts {
+
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+
+    public static class Builder {
+        private final int servingSize;
+        private final int servings;
+
+        private int calories = 0;
+        private int fat = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder calories (int val){
+            calories = val;
+            return this;
+        }
+
+        public Builder fat (int val) {
+            fat = val;
+            return this;
+        }
+
+        public NutritionFacts build(){
+            return new NutritionFacts(this);
+        }
+    }
+
+    private NutritionFacts(Builder builder){
+        servingSize = builder.servingSize;
+        servings = builder.servings;
+        fat = builder.fat;
+        calories = builder.calories;
+    }
+}
