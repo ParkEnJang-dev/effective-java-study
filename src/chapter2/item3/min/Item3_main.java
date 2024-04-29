9bff0327043b5418e15ea62f377a9fb32e620f0d
min-gui <msj349911@gmail.com>
2024-04-29 21:34:10 +0900

    min item4

--- src/chapter2/item3/min/Item3_main.java
+++ src/chapter2/item3/min/Item3_main.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class Item3_main {
 
-    public static void main(String[] args) {
-        // 외부에서 변경가능
+    public static void main(String[] args) throws ClassNotFoundException {
+        /*// 외부에서 변경가능
         Elvis.instance = null;
 
         // Thread safe 하지 않음. (상위 주석 후 수차례 반복 시 확인 가능)
@@ -44,6 +44,16 @@ public class Item3_main {
         for ( int i = 0; i < threads.size(); i++ ) {
             Thread thread = threads.get( i );
             thread.start();
-        }
+        }*/
+
+        Class<Member> memberClass = Member.class;
+        System.out.println(System.identityHashCode(memberClass));
+
+        Member member = new Member("제이온");
+        Class<? extends Member> memberClass2 = member.getClass();
+        System.out.println(System.identityHashCode(memberClass2));
+
+        Class<?> memberClass3 = Class.forName("chapter2.item3.min.Member");
+        System.out.println(System.identityHashCode(memberClass3));
     }
 }
---
+++ src/chapter2/item3/min/Member.java
@@ -0,0 +1,11 @@
+package chapter2.item3.min;
+
+public class Member {
+
+    private String name;
+
+    Member(String name) {
+        this.name = name;
+    }
+
+}
